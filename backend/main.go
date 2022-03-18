d3cd69b85def15998e55b981764f171cf5d31eb1
1shikawa <ishikawa.toru@gmail.com>
2022-03-18 23:21:28 +0900

    fix main

--- backend/main.go
+++ backend/main.go
@@ -12,7 +12,7 @@ func main() {
     router.Use(cors.New(cors.Config{
     // アクセスを許可したいアクセス元
     AllowOrigins: []string{
-        'http://localhost:4000'
+        'http://localhost:4000',
     },
     // アクセスを許可したいHTTPメソッド(以下の例だとPUTやDELETEはアクセスできません)
     AllowMethods: []string{

f4d187182f753326f16ba397563fa813cf145eff
1shikawa <ishikawa.toru@gmail.com>
2022-03-18 23:15:10 +0900

    add cors settings

--- backend/main.go
+++ backend/main.go
@@ -2,10 +2,38 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 func main() {
 	router := gin.Default()
+
+	 // ここからCorsの設定
+    router.Use(cors.New(cors.Config{
+    // アクセスを許可したいアクセス元
+    AllowOrigins: []string{
+        'http://localhost:4000',
+    },
+    // アクセスを許可したいHTTPメソッド(以下の例だとPUTやDELETEはアクセスできません)
+    AllowMethods: []string{
+        "POST",
+        "GET",
+        "OPTIONS",
+    },
+    // 許可したいHTTPリクエストヘッダ
+    AllowHeaders: []string{
+        "Access-Control-Allow-Credentials",
+        "Access-Control-Allow-Headers",
+        "Content-Type",
+        "Content-Length",
+        "Accept-Encoding",
+        "Authorization",
+    },
+    // cookieなどの情報を必要とするかどうか
+    AllowCredentials: true
+    // preflightリクエストの結果をキャッシュする時間
+    MaxAge: 24 * time.Hour,
+}))
 
 	router.GET("/hello", func(c *gin.Context){
 		c.JSON(200, gin.H{
